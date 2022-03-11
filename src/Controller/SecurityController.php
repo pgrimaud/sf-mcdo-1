530799a1105b90359f3debd069e72aa738863c67
Pierre Grimaud <grimaud.pierre@gmail.com>
2022-03-11 10:13:58 +0100

    Add user login form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,16 +12,70 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('@EasyAdmin/page/login.html.twig', [
+            // parameters usually defined in Symfony login forms
+            'error' => $error,
+            'last_username' => $lastUsername,
+
+            // OPTIONAL parameters to customize the login form:
+
+            // the translation_domain to use (define this option only if you are
+            // rendering the login template in a regular Symfony controller; when
+            // rendering it from an EasyAdmin Dashboard this is automatically set to
+            // the same domain as the rest of the Dashboard)
+            'translation_domain' => 'admin',
+
+            // the title visible above the login form (define this option only if you are
+            // rendering the login template in a regular Symfony controller; when rendering
+            // it from an EasyAdmin Dashboard this is automatically set as the Dashboard title)
+            'page_title' => '<img style="width:150px;" src="https://scontent-frx5-2.xx.fbcdn.net/v/t1.6435-9/66252303_2260659157322981_4090138080860700672_n.png?_nc_cat=1&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=8v80SFsoPhYAX9hQgQr&_nc_ht=scontent-frx5-2.xx&oh=00_AT_e5ztUEZcoOaJhUME7bVMgHforB9IXFLxxvYR6srJXnQ&oe=62505DB0">',
+
+            // the string used to generate the CSRF token. If you don't define
+            // this parameter, the login form won't include a CSRF token
+            'csrf_token_intention' => 'authenticate',
+
+            // the URL users are redirected to after the login (default: '/admin')
+            'target_path' => $this->generateUrl('admin'),
+
+            // the label displayed for the username form field (the |trans filter is applied to it)
+            'username_label' => 'Email',
+
+            // the label displayed for the password form field (the |trans filter is applied to it)
+            'password_label' => 'Password',
+
+            // the label displayed for the Sign In form button (the |trans filter is applied to it)
+            'sign_in_label' => 'Log in',
+
+            // the 'name' HTML attribute of the <input> used for the username field (default: '_username')
+            'username_parameter' => 'email',
+
+            // the 'name' HTML attribute of the <input> used for the password field (default: '_password')
+            'password_parameter' => 'password',
+
+            // whether to enable or not the "forgot password?" link (default: false)
+            'forgot_password_enabled' => false,
+
+            // the path (i.e. a relative or absolute URL) to visit when clicking the "forgot password?" link (default: '#')
+            'forgot_password_path' => $this->generateUrl('admin', ['...' => '...']),
+
+            // the label displayed for the "forgot password?" link (the |trans filter is applied to it)
+            'forgot_password_label' => 'Forgot your password?',
+
+            // whether to enable or not the "remember me" checkbox (default: false)
+            'remember_me_enabled' => true,
+
+            // remember me name form field (default: '_remember_me')
+            'remember_me_parameter' => 'custom_remember_me_param',
+
+            // whether to check by default the "remember me" checkbox (default: false)
+            'remember_me_checked' => true,
+
+            // the label displayed for the remember me checkbox (the |trans filter is applied to it)
+            'remember_me_label' => 'Remember me',
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
