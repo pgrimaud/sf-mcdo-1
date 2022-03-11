35b1f26aecb21e0d29d7ee8a1483aaff91a7382c
Pierre Grimaud <grimaud.pierre@gmail.com>
2022-03-11 12:14:37 +0100

    Add navbar

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\DistrictRepository;
+use App\Repository\ProductRepository;
 use App\Repository\RestaurantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,14 +12,30 @@ use Symfony\Component\Routing\Annotation\Route;
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(RestaurantRepository $restaurantRepository): Response
+    public function index(
+        RestaurantRepository $restaurantRepository
+    ): Response
     {
         $restaurants = $restaurantRepository->findBy([], [
             'createdAt' => 'DESC'
         ], 20);
 
         return $this->render('app/index.html.twig', [
-            'restaurants' => $restaurants
+            'restaurants' => $restaurants,
+        ]);
+    }
+
+    public function nav(
+        DistrictRepository $districtRepository,
+        ProductRepository  $productRepository
+    )
+    {
+        $districts = $districtRepository->findAll();
+        $products = $productRepository->findAll();
+
+        return $this->render('partial/nav.html.twig', [
+            'districts' => $districts,
+            'products' => $products
         ]);
     }
 }
